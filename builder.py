dd287164c6a8a0325e760e72f0a352f99c9cf269
roure <rourexs@gmail.com>
2020-12-24 23:14:09 +0100

    first pass almost done (wip) toDo are missing

--- builder.py
+++ builder.py
@@ -1,17 +1,35 @@
 import maya.cmds as mc
-import maya.mel as mel
+import types
+import keyword
 
+with open(r'C:\Users\roure\Downloads\__init__.py', 'w+') as commands_file:
+    for command in dir(mc):
+        if not isinstance(getattr(mc, command), types.BuiltinFunctionType):
+            continue
+        try:
+            help_string = mc.help(command, syntaxOnly=True)
+        except RuntimeError:
+            continue
 
-for command in dir(mc):
-    help_string = str(mel.eval('help("{}")'.format(command)))
-    if "No Flags" in help_string:
-        continue
-    # toDo: extract the string following the "-" caracter
-    
-    command_line = """
-def {}(, *args, **kwargs):
+        if "Flags:\n" not in help_string:
+            command_line = """
+def {}(*args, **kwargs):
     pass
 """.format(command)
-    
-    # open an write the new command to the end of the file.
+        else:
+            args = list()
+            for arg in help_string.split(' '):
+                if not arg or not len(arg.strip()) > 1 or not '-' == arg[0]:
+                    continue
+                arg = arg.strip()[1:]
 
+                if arg in ['e', 'edit', 'q', 'query'] or keyword.iskeyword(arg):
+                    continue
+
+                args.append(arg)
+            # toDo: if not args (because only query and edit are known) use the other command_line format
+            command_line = """
+def {}({}*args, **kwargs):
+    pass
+""".format(command, '=None, '.join(args) + '=None, ')
+        commands_file.write(command_line)
